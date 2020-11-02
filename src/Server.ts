a608a6336056b4f81708bae84ace3610f278a061
Rusinov Dmitriy Andreevich <DRusinov@gksm.local>
2020-11-02 16:33:01 +0300

    fix acces cors policy

--- src/Server.ts
+++ src/Server.ts
@@ -10,40 +10,46 @@ import 'express-async-errors';
 import BaseRouter from './routes';
 import logger from '@shared/Logger';
 
-
 // Init express
 const app = express();
-
-
 
 /************************************************************************************
  *                              Set basic express settings
  ***********************************************************************************/
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 // Show routes called in console during development
 if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'));
+  app.use(morgan('dev'));
 }
 
 // Security
 if (process.env.NODE_ENV === 'production') {
-    app.use(helmet());
+  app.use(helmet());
 }
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  next();
+});
 
 // Add APIs
 app.use('/api', BaseRouter);
 
 // Print API errors
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-    logger.error(err.message, err);
-    return res.status(BAD_REQUEST).json({
-        error: err.message,
-    });
+  logger.error(err.message, err);
+  return res.status(BAD_REQUEST).json({
+    error: err.message,
+  });
 });
-
 
 // Export express instance
 export default app;
